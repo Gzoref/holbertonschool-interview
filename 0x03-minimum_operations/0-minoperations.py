40a221b9349854b7f51dc0b8da0faee05179dcf9
Gzoref <gzoref@gmail.com>
2020-08-07 14:56:14 -0400

    Return smallest # of oeprations to print n Hs

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
+
+
+def minOperations(n):
+    '''
+    returns min operations to get n Hs
+    '''
+    result = 0
+    for i in range(2, n):
+        # Check if problem is evenly brekadownable
+        while n % i == 0:
+            # If so add number of smaller problems to the result
+            result += i
+            # Create the smaller problem needed to get to n
+            n /= i
+    return result
