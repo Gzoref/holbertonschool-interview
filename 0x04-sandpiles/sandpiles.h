588225932a94cc1b2f830f0c3e059b777dca6cdf
Gzoref <gzoref@gmail.com>
2020-08-12 22:58:18 -0400

    Add print function to header

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,9 @@
-#ifdef _SANDPILES_
-#define _SANDPILES_
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+#include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
 
-#endif /* _SANDPILES */
+#endif /* _SANDPILES_H */
