4fc3cd412c037d380b83504386896c6d0bd66f4e
Gzoref <gzoref@gmail.com>
2020-08-15 17:53:13 -0400

    Fix header giard

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,12 +1,12 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
+void _print_grid(int grid[3][3]);
 int is_stable(int grid1[3][3]);
 void topple(int grid1[3][3], int grid2[3][3]);
 
-#endif /* SANDPILES_H */
+#endif /* _SANDPILES_H_ */
