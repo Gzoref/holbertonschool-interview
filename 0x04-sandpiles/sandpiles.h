6f07c07ef878cf304571057c672eeef448a1fc45
Gzoref <gzoref@gmail.com>
2020-08-15 17:07:30 -0400

    Fix header guard

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,12 @@
-#ifndef _SANDPILES_H_
-#define _SANDPILES_H_
+#ifndef SANDPILES_H
+#define SANDPILES_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void print_grid(int grid[3][3]);
+int is_stable(int grid1[3][3]);
+void topple(int grid1[3][3], int grid2[3][3]);
 
-#endif /* _SANDPILES_H */
+#endif /* SANDPILES_H */
