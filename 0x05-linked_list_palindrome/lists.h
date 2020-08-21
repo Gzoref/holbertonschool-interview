348aefeab45ca76cf14bd69e48cd10566b8a494e
Gzoref <gzoref@gmail.com>
2020-08-21 14:50:32 -0400

    Update header

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int recurse_list(listint_t **left, listint_t *right);
 
 #endif /* LISTS_H */
