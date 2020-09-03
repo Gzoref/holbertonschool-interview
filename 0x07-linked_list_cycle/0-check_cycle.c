313b0bf4a804e11f15e9bed7141d31942e950599
Gzoref <gzoref@gmail.com>
2020-09-03 10:26:08 -0400

    Fix checing for cycle after slow and fast have started moving

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,23 +12,19 @@ int check_cycle(listint_t *list)
 {
 	/* Initialize fast at the next node up from slow */
 	listint_t *slow = list;
-	listint_t *fast = list->next;
-
-	if (fast && fast->next != NULL && slow)
-	{
-		return (0);
-	}
+	listint_t *fast = list;
 
 	while (slow && fast)
 	{
+		/* slow increments by one node, fast by 2 nodes */
+		slow =  slow->next;
+		fast = fast->next->next;
+
 		/* If the collide than a cycle is found */
-		if (slow->n == fast->n)
+		if (slow == fast)
 		{
 			return (1);
 		}
-		/* slow increments by one node, fast by 2 nodes */
-			slow =  slow->next;
-			fast = fast->next->next;
 	}
 	return (0);
-}
+	}
