b7009714f7966c14645668f7ec6d3db0837f007e
Gzoref <gzoref@gmail.com>
2020-09-03 13:13:30 -0400

    Fix indent issues on method

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,38 +7,36 @@ Reads stdin line by line and computes metrics
 '''
 if __name__ == "__main__":
 
-    file_size = [0]
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
-
+    file_size = [0]
     count = 1
-    file_size = 0
 
     def print_stats():
         '''
         Prints file size and stats for every 10 loops
         '''
-    print('File size: {}'.format(file_size))
+        print('File size: {}'.format(file_size[0]))
 
-    for code in sorted(status_codes.keys()):
-        if status_codes[code]:
-            print('{} {}', format(status_codes, status_codes[code]))
+        for code in sorted(status_codes.keys()):
+            if status_codes[code]:
+                print('{} {}', format(status_codes, status_codes[code]))
 
     def parse_stdin(line):
         '''
         Checks the stdin for matches
         '''
-    try:
-        line = line[:-1]
-        word = line.split(' ')
-        # File size is last parameter on stdout
-        file_size[0] += int(word[-1])
-        # Status code comes before file size
-        status_code = int(word[-2])
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-    except BaseException:
-        pass
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
     try:
         for line in sys.stdin:
