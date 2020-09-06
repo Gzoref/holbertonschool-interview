76417b54572db96dae72994976571107ee4812ff
Gzoref <gzoref@gmail.com>
2020-09-06 15:56:55 -0400

    Add condition to while loop

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -10,21 +10,20 @@
 
 int check_cycle(listint_t *list)
 {
-	/* Initialize fast at the next node up from slow */
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slow && fast)
+	while (slow && fast && fast->next)
 	{
-		/* slow increments by one node, fast by 2 nodes */
-		slow =  slow->next;
+		/* slow increments by one node, fast by two nodes */
+		slow = slow->next;
 		fast = fast->next->next;
 
-		/* If the collide than a cycle is found */
+		/* If they collide than a cycle is found */
 		if (slow == fast)
 		{
 			return (1);
 		}
 	}
 	return (0);
-	}
+}
