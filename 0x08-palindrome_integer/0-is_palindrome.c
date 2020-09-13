c4a6f2e042ab446405d9d9e4fa453bba062ee192
Gzoref <gzoref@gmail.com>
2020-09-13 18:10:06 -0400

    Return 1 for 1 dgit numbers

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -14,7 +14,12 @@ int is_palindrome(unsigned long n)
 	int remainder = 0;
 	unsigned long reverse = 0;
 
-	while (n != 0)
+	if (n < 9)
+	{
+		return (1);
+	}
+
+	while (n > 0)
 	{
 		/* Get last digit in n */
 		remainder = n % 10;
