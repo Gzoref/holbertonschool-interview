713a3e2ba4ad6d216b9124cee17e9c64ef20a8bb
Gzoref <gzoref@gmail.com>
2020-09-28 14:58:20 -0400

    Initial file creation

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,18 @@
+#include "slide_line.h"
+
+/**
+* slide_line - Reproduces 2048 on one line
+*
+* @line: Points to an array of integers containing size elements
+*
+* @size: Size of the array
+*
+* @direction: Direction to slide
+*
+* Return: 1 on success, 0 on failure
+*/
+
+int slide_line(int *line, size_t size, int direction)
+{
+
+}
