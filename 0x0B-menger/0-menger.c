841b320375a3c9cb2e762a2769f138cd78d3007f
Gzoref <gzoref@gmail.com>
2020-10-04 15:52:44 -0400

    Add function comment

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,6 +1,25 @@
 #include "menger.h"
 
+/**
+* menger - Prints an N sized Menger Sponge
+*
+* @level: Menger level to print
+*
+* Return: Void
+*/
+
 void menger(int level)
 {
-	
+
+	int size = level * 3;
+
+	if (level < 0)
+	{
+		return;
+	}
+
+	if (level == 0)
+	{
+		printf("#");
+	}
 }
