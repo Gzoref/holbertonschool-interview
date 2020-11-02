cb46d23d022b4df94a40bc0d7ff482f812ec0a62
Gzoref <gzoref@gmail.com>
2020-11-01 19:09:10 -0500

    AVL - From sorted array

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -26,5 +26,8 @@ typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *insert_sorted_array(int *array, int min, int max);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif /* _BINARY_TREES_H_ */
