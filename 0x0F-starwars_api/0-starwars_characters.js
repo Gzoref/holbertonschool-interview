6537170b42a6ae721914a9b03824dafdfed894d6
Gzoref <gzoref@gmail.com>
2020-11-12 17:29:11 -0500

    Change functions to arrow notation

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,13 +4,13 @@ const request = require('request');
 const filmId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-request(url, async function (err, response, body) {
+request(url, async (err, response, body) => {
   if (err) {
     console.log(err);
   }
   for (const characterId of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
-      request(characterId, function (err, response, body) {
+      request(characterId, (err, response, body) => {
         if (err) {
           reject(err);
         }
