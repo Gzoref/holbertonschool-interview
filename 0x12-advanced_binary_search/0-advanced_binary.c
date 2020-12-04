460a1fc5a8da8ab5df80231136034fc1474af192
Gzoref <gzoref@gmail.com>
2020-12-04 15:01:32 -0500

    Create files  Advanced Binary Search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+
+/**
+* advanced_binary - Searches for a value in a sorted array of integers
+*
+* @array: Pointer to the first element of the array
+*
+* @size: Number of elements in the array
+*
+* @value: Value to search for
+*
+* Return: Value searched for or -1 if aray is NULL
+*/
+
+int advanced_binary(int *array, size_t size, int value)
+{
+
+}
