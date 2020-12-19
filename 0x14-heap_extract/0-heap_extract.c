731501f42da7785deb58a68445549cd730abefe9
Gzoref <gzoref@gmail.com>
2020-12-19 17:45:41 -0500

    Create files

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - Extracts the root node of a Max Binary Heap
+*
+* @root: Double Pointer to root node of heap
+*
+* Return: Value stored in the root node
+*/
+
+int heap_extract(heap_t **root)
+{
+
+}
