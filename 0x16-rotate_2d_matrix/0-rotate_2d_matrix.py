e65f2fef960561aef74f3894dc89d17e6d490bf0
Gzoref <gzoref@gmail.com>
2021-01-19 16:36:05 -0500

    Pep8 fixes

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -16,11 +16,7 @@ def rotate_2d_matrix(matrix):
     for i in range(0, int(row / 2)):
         for j in range(i, column - i):
             temp = matrix[i][j]
-
             matrix[i][j] = matrix[column - j][i]
-            
             matrix[column - j][i] = matrix[column - i][column - j]
-            
             matrix[column - i][column - j] = matrix[j][column - i]
-            
             matrix[j][column - i] = temp
